a73d8516221a907944eefc76dd8b061cb6eb5e21
Jacob Matthews <jacob@tockhq.com>
2017-09-27 22:46:10 -0500

    More JSON reading helpers

--- src/SimpleDecoder.elm
+++ src/SimpleDecoder.elm
@@ -1,8 +1,28 @@
-module SimpleDecoder exposing (JsonValue(..), readJsonValue, toJsonValue, wrapPort, at)
+module SimpleDecoder
+    exposing
+        ( JsonValue(..)
+        , readJsonValue
+        , toJsonValue
+        , wrapPort
+        , SDecoder
+        , string
+        , int
+        , float
+        , bool
+        , null
+        , field
+        , at
+        , map
+        , oneOf
+        , andThen
+        )
 
 {-| Library for writing simple JSON decoders.
 
-@docs JsonValue, readJsonValue, toJsonValue, wrapPort, at
+@docs JsonValue, readJsonValue, toJsonValue, wrapPort
+
+Functions and types for manipulating JsonValue values:
+@docs SDecoder, string, int, float, bool, null, field, at, map, oneOf, andThen
 
 -}
 
@@ -106,13 +126,87 @@ wrapPort p simpleDecoder =
     p (readJsonValue >> simpleDecoder)
 
 
-field : String -> JsonValue -> Result String JsonValue
-field name jsonValue =
+{-| Simple type alias for functions that parse a JsonValue into a value of
+some arbitrary type t. Since the parse may fail, the function returns a
+Result that could indicate a parse error.
+-}
+type alias SDecoder t =
+    JsonValue
+    -> Result String t
+
+
+{-| Decodes a JSON string to an elm string value.
+-}
+string : SDecoder String
+string jsonValue =
+    case jsonValue of
+        String s ->
+            Ok s
+
+        _ ->
+            Err "Expected a string"
+
+
+{-| Decodes a JSON int to an elm int value.
+-}
+int : SDecoder Int
+int jsonValue =
+    case jsonValue of
+        Int i ->
+            Ok i
+
+        _ ->
+            Err "Expected an integer"
+
+
+{-| Decodes a JSON number to an elm float value.
+-}
+float : SDecoder Float
+float jsonValue =
+    case jsonValue of
+        Int i ->
+            Ok (toFloat i)
+
+        Float f ->
+            Ok f
+
+        _ ->
+            Err "Expected a number"
+
+
+{-| Decodes a JSON bool to an elm bool value.
+-}
+bool : SDecoder Bool
+bool jsonValue =
+    case jsonValue of
+        Bool b ->
+            Ok b
+
+        _ ->
+            Err "Expected a boolean"
+
+
+{-| Decodes JSON null to a supplied elm value.
+-}
+null : t -> SDecoder t
+null val jsonValue =
+    case jsonValue of
+        Null ->
+            Ok val
+
+        _ ->
+            Err "Expected null"
+
+
+{-| Returns the value at the field with the given name.
+-}
+field : String -> SDecoder t -> SDecoder t
+field name decoder jsonValue =
     case jsonValue of
         Obj dict ->
             case Dict.get name dict of
                 Just fieldValue ->
-                    Ok fieldValue
+                    decoder fieldValue
 
                 Nothing ->
                     Err ("Field name " ++ name ++ " not found")
@@ -123,29 +217,24 @@ field name jsonValue =
 
 {-| Decodes a nested JSON object, following the given fields.
 -}
-at : List String -> JsonValue -> Result String JsonValue
-at path v =
+at : List String -> SDecoder t -> SDecoder t
+at path decoder v =
     case path of
         [] ->
-            Ok v
+            decoder v
 
         first :: rest ->
             case v of
                 Obj d ->
                     case Dict.get first d of
                         Just subvalue ->
-                            at rest subvalue
+                            at rest decoder subvalue
 
                         Nothing ->
                             Err ("Subpath " ++ first ++ " not found")
 
                 _ ->
                     Err "Encountered a non-object"
-
-
-type alias SDecoder t =
-    JsonValue
-    -> Result String t
 
 
 orElse : (() -> Result x y) -> Result x y -> Result x y
@@ -158,12 +247,18 @@ orElse thunk result =
             thunk ()
 
 
+{-| Returns a decoder that returns the result of applying the given
+function to the successful result of decoding using the given decoder.
+-}
 map : (s -> t) -> SDecoder s -> SDecoder t
 map f decoder jsonValue =
     decoder jsonValue
         |> Result.map f
 
 
+{-| Returns a decoder that tries each of the given decoders in turn when
+parsing its input, and that fails only if every decoder fails.
+-}
 oneOf : List (SDecoder t) -> SDecoder t
 oneOf decoders jsonValue =
     case decoders of
@@ -174,6 +269,12 @@ oneOf decoders jsonValue =
             decoder jsonValue |> orElse (\_ -> oneOf rest jsonValue)
 
 
+{-| Returns a decoder that runs the given decoder against its input. Then,
+if the decode is successful, it applies the given function and re-parses
+the input JSON against the resulting second decoder. This allows a decoder
+that reads part of the JSON object before deciding how to parse the rest
+of the object.
+-}
 andThen : (a -> SDecoder b) -> SDecoder a -> SDecoder b
 andThen toB aDecoder jsonValue =
     case aDecoder jsonValue of
--- tests/SimpleDecoderTests.elm
+++ tests/SimpleDecoderTests.elm
@@ -6,7 +6,22 @@ import Fuzz exposing (Fuzzer, float, int, list, string)
 import Test exposing (..)
 import Json.Encode as Encode
 import JsonUtil exposing (jsonFromString)
-import SimpleDecoder exposing (..)
+import SimpleDecoder exposing (JsonValue(..), readJsonValue)
+
+
+constructValueAtPath : JsonValue -> List String -> JsonValue
+constructValueAtPath value path =
+    case path of
+        [] ->
+            value
+
+        first :: rest ->
+            Obj (Dict.fromList [ ( first, constructValueAtPath value rest ) ])
+
+
+readJsonFromString : String -> JsonValue
+readJsonFromString =
+    jsonFromString >> readJsonValue
 
 
 suite : Test
@@ -37,7 +52,7 @@ suite =
                     Expect.equal Null (readJsonValue Encode.null)
             , test "Decodes objects" <|
                 \_ ->
-                    case readJsonValue (jsonFromString "{\"x\": 17}") of
+                    case readJsonFromString "{\"x\": 17}" of
                         Obj d ->
                             Expect.true "Expects to have member named 'x'" (Dict.member "x" d)
 
@@ -45,7 +60,7 @@ suite =
                             Expect.fail "Expected an object"
             , test "Decodes arrays" <|
                 \_ ->
-                    case readJsonValue (jsonFromString "[1,2,3,4]") of
+                    case readJsonFromString "[1,2,3,4]" of
                         Arr lst ->
                             Expect.equal [ Int 1, Int 2, Int 3, Int 4 ] lst
 
@@ -58,9 +73,51 @@ suite =
                             "{\"data\":{\"type\":\"gif\",\"id\":\"3oEjHSaIF3Oo9LjjXO\",\"url\":\"http://giphy.com/gifs/tacocatband-dana-hardly-art-tacocat-3oEjHSaIF3Oo9LjjXO\",\"image_original_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/giphy.gif\",\"image_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/giphy.gif\",\"image_mp4_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/giphy.mp4\",\"image_frames\":\"14\",\"image_width\":\"450\",\"image_height\":\"255\",\"fixed_height_downsampled_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/200_d.gif\",\"fixed_height_downsampled_width\":\"353\",\"fixed_height_downsampled_height\":\"200\",\"fixed_width_downsampled_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/200w_d.gif\",\"fixed_width_downsampled_width\":\"200\",\"fixed_width_downsampled_height\":\"113\",\"fixed_height_small_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/100.gif\",\"fixed_height_small_still_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/100_s.gif\",\"fixed_height_small_width\":\"176\",\"fixed_height_small_height\":\"100\",\"fixed_width_small_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/100w.gif\",\"fixed_width_small_still_url\":\"https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/100w_s.gif\",\"fixed_width_small_width\":\"100\",\"fixed_width_small_height\":\"57\",\"username\":\"tacocatband\",\"caption\":\"\"},\"meta\":{\"status\":200,\"msg\":\"OK\",\"response_id\":\"59cc4fae446c536c7742d0b2\"}}"
 
                         parsed =
-                            readJsonValue (jsonFromString giphyString)
+                            readJsonFromString giphyString
                     in
-                        Expect.equal (at [ "data", "image_url" ] parsed)
+                        Expect.equal (SimpleDecoder.at [ "data", "image_url" ] Ok parsed)
                             (Ok (String "https://media2.giphy.com/media/3oEjHSaIF3Oo9LjjXO/giphy.gif"))
+            , fuzz (list string) "Test at on arbitrary paths" <|
+                \path ->
+                    let
+                        nestedValue =
+                            String "testValue"
+
+                        jsonValue =
+                            constructValueAtPath nestedValue path
+                    in
+                        Expect.equal (Ok nestedValue) (SimpleDecoder.at path Ok jsonValue)
+            , fuzz string "string decoder" <|
+                \s ->
+                    Expect.equal (Ok s) (SimpleDecoder.string (String s))
+            , fuzz int "int decoder" <|
+                \i ->
+                    Expect.equal (Ok i) (SimpleDecoder.int (Int i))
+            , fuzz float "float decoder on float input" <|
+                \f ->
+                    Expect.equal (Ok f) (SimpleDecoder.float (Float f))
+            , fuzz int "float decoder on int input" <|
+                \i ->
+                    Expect.equal (Ok (toFloat i)) (SimpleDecoder.float (Int i))
+            , test "oneOf" <|
+                \_ ->
+                    SimpleDecoder.oneOf
+                        [ SimpleDecoder.int
+                        , SimpleDecoder.at [ "a", "b" ] SimpleDecoder.int
+                        ]
+                        (readJsonFromString "{\"a\": {\"b\": 12}}")
+                        |> Expect.equal (Ok 12)
+            , let
+                decoder =
+                    SimpleDecoder.field "interestingVar" SimpleDecoder.string
+                        |> SimpleDecoder.andThen
+                            (\interestingVar ->
+                                SimpleDecoder.field interestingVar SimpleDecoder.int
+                            )
+              in
+                test "andThen" <|
+                    \_ ->
+                        decoder (readJsonFromString "{\"interestingVar\":\"b\",\"a\":10,\"b\":12}")
+                            |> Expect.equal (Ok 12)
             ]
         ]
